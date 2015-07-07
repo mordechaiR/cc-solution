fc381c158241c3d96b777e2c9a138dc6fcd4f49c
mordechaiR <mrorvig@gmail.com>
2015-07-07 13:32:27 -0500

    Added comments to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# bash script to execute program
 INPUTPATH=./tweet_input/shakes.txt
-g++ -std=c++11 src/main.cpp 
+# compile sourcefile using g++ compiler, ignore C++ 11 warnings
+g++ -std=c++11 src/main.cpp
+# run program using INPUTPATH as command line argument
 ./a.out $INPUTPATH
